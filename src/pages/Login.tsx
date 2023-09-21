b273cfca4d37c9e7bf5d41e6bb7761b31e971f36
azizan nur rohman <zhiz.nevermind@gmail.com>
2023-09-21 09:25:18 +0700

    checked login failed

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -6,23 +6,33 @@ const Login = (props: {setName: (name: string) => void}) => {
     const [password, setPassword] = useState('');
     const navigate = useNavigate();
     const [redirect, setRedirect] = useState(false);
+    const [error, setError] = useState<string | null>(null);
 
     const submit = async (e: SyntheticEvent) => {
         e.preventDefault();
 
-        const response = await fetch('http://localhost:8000/api/login', {
-            method: 'POST',
-            headers: {'Content-Type': 'application/json'},
-            credentials: 'include',
-            body: JSON.stringify({
-                email,
-                password
-            })
-        });
+        try {
+            const response = await fetch('http://localhost:8000/api/login', {
+                method: 'POST',
+                headers: {'Content-Type': 'application/json'},
+                credentials: 'include',
+                body: JSON.stringify({
+                    email,
+                    password
+                })
+            });
 
-        const content = await response.json();
-        props.setName(content.name);
-        setRedirect(true);
+            if (response.ok) {
+                const content = await response.json();
+                props.setName(content.name);
+                setRedirect(true);
+            }else{
+                setError('Login failed, plese try again');
+            }
+        } catch (error) {
+            // Tangani kesalahan jaringan atau kesalahan lainnya di sini
+            setError('Login failed, plese try again');
+          }
     }
 
     if(redirect){
@@ -36,6 +46,7 @@ const Login = (props: {setName: (name: string) => void}) => {
                 <input type="email" className="form-control" id="floatingInput" placeholder="name@example.com" onChange={e => setEmail(e.target.value)} />
                 <input type="password" className="form-control" id="floatingPassword" placeholder="Password" onChange={e => setPassword(e.target.value)} />
             </div>
+            {error && <div className="alert alert-danger">{error}</div>}
             <button className="btn btn-primary w-100 py-2" type="submit">Sign in</button>
         </form>
     );
